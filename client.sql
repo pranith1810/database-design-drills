3de4dfb5bf49071c4834f5f69117e79bdfa5b631
pranith9810 <n.rao@mountblue.tech>
2020-08-14 22:15:39 +0530

    Changed the create table queries in client.sql file to have a manager for each staff and changed all the primary keys to support uuids

--- client.sql
+++ client.sql
@@ -1,31 +1,35 @@
 CREATE TABLE Location(
-Location_id INT PRIMARY KEY,
+Location_id VARCHAR(36) PRIMARY KEY,
 Location_name TEXT
 );
 
 CREATE TABLE Manager(
-Manager_id INT PRIMARY KEY,
+Manager_id VARCHAR(36) PRIMARY KEY,
 Manager_name TEXT, 
-Mnager_location_id INT REFERENCES Location(Location_id)
+Manager_location_id VARCHAR(36) REFERENCES Location(Location_id)
+);
+
+CREATE TABLE Staff(
+Staff_id VARCHAR(36) PRIMARY KEY,
+Staff_name TEXT,
+Staff_location_id VARCHAR(36) REFERENCES Location(Location_id),
+Manager_id VARCHAR(36) REFERENCES Manager(Manager_id)
 );
 
 CREATE TABLE Contract(
-Contract_id INT PRIMARY KEY,
+Contract_id VARCHAR(36) PRIMARY KEY,
 Estimated_cost INT,
 Completion_date DATE 
 );
 
-CREATE TABLE Staff(
-Staff_id INT PRIMARY KEY,
-Staff_name TEXT,
-Staff_location_id INT REFERENCES Location(Location_id)
+CREATE TABLE Client(
+Client_id VARCHAR(36) PRIMARY KEY,
+Client_name TEXT,
+Client_location_id VARCHAR(36) REFERENCES Location(Location_id)
 );
 
-CREATE TABLE Client(
-Client_id INT PRIMARY KEY,
-Client_name TEXT,
-Location_id INT REFERENCES Location(Location_id),
-Manager_id  INT REFERENCES Manager(Manager_id),
-Contract_id INT REFERENCES Contract(Contract_id),
-Staff_id INT REFERENCES Staff(Staff_id)
+CREATE TABLE clients_contracts(
+client_id VARCHAR(36) REFERENCES Client(Client_id),
+contract_id VARCHAR(36) REFERENCES Contract(Contract_id),
+manager_id VARCHAR(36) REFERENCES Manager(Manager_id)
 );
