93d87da5e5a31b0c0a8d7986f329098f13619f21
pranith9810 <n.rao@mountblue.tech>
2020-08-14 23:22:20 +0530

    Changed the create table queries in doctor.sql and patient.sql file to normalize them more by adding separate table for drugs

--- doctor.sql
+++ doctor.sql
@@ -1,25 +1,31 @@
 CREATE TABLE Doctor(
-Doctor_id INT PRIMARY KEY,
+Doctor_id VARCHAR(36) PRIMARY KEY,
 Doctor_name TEXT,
-Secretary TEXT
+Secretary_name TEXT
+);
+
+CREATE TABLE Drug(
+Drug_id VARCHAR(36) PRIMARY KEY,
+Drug_name TEXT,
+Brand_name TEXT
 );
 
 CREATE TABLE Prescription(
-Prescription_id INT PRIMARY KEY,
-Drug TEXT,
+Prescription_id VARCHAR(36) PRIMARY KEY,
+Drug VARCHAR(36) REFERENCES Drug_id,
 Date_given DATE,
-Dosage INT
+Dosage TEXT
 );
 
 CREATE TABLE Patient(
-Patient_id INT PRIMARY KEY,
+Patient_id VARCHAR(36) PRIMARY KEY,
 Name TEXT,
 DOB DATE,
 Address TEXT
 );
 
 CREATE TABLE Doctor_patient(
-Doctor_id INT REFERENCES Doctor(Doctor_id),
-Patient_id INT REFERENCES Patient(Patient_id),
-Prescription_id INT REFERENCES Prescription(Prescription_id)
+Doctor_id VARCHAR(36) REFERENCES Doctor(Doctor_id),
+Patient_id VARCHAR(36) REFERENCES Patient(Patient_id),
+Prescription_id VARCHAR(36) REFERENCES Prescription(Prescription_id)
 );
--- patient.sql
+++ patient.sql
@@ -1,21 +1,31 @@
 CREATE TABLE Doctor(
-Doctor_id INT PRIMARY KEY,
+Doctor_id VARCHAR(36) PRIMARY KEY,
 Doctor_name TEXT,
-Secretary TEXT
+Secretary_name TEXT
+);
+
+CREATE TABLE Drug(
+Drug_id VARCHAR(36) PRIMARY KEY,
+Drug_name TEXT,
+Brand_name TEXT
 );
 
 CREATE TABLE Prescription(
-Prescription_id INT PRIMARY KEY,
-Drug TEXT,
+Prescription_id VARCHAR(36) PRIMARY KEY,
+Drug VARCHAR(36) REFERENCES Drug_id,
 Date_given DATE,
-Dosage INT
+Dosage TEXT
 );
 
 CREATE TABLE Patient(
-Patient_id INT PRIMARY KEY,
-Patient_name TEXT,
+Patient_id VARCHAR(36) PRIMARY KEY,
+Name TEXT,
 DOB DATE,
-Address TEXT,
+Address TEXT
+);
+
+CREATE TABLE Patient_doctor(
+Patient_id INT PRIMARY KEY,
 Prescription_id INT REFERENCES Prescription(Prescription_id),
 Doctor_id INT REFERENCES Doctor(Doctor_id)
 );
