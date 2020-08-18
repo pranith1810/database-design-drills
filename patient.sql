964972a851688f1188b55016f0a292531f6916ec
pranith9810 <n.rao@mountblue.tech>
2020-08-18 15:20:01 +0530

    Added multiple drug and dosage columns while creating table Prescription in doctor.sql and patient.sql files

--- patient.sql
+++ patient.sql
@@ -12,9 +12,17 @@ Brand_name TEXT
 
 CREATE TABLE Prescription(
 Prescription_id VARCHAR(36) PRIMARY KEY,
-Drug VARCHAR(36) REFERENCES Drug_id,
-Date_given DATE,
-Dosage TEXT
+Drug1 VARCHAR(36) REFERENCES Drug_id,
+Dosage_drug1 TEXT,
+Drug2 VARCHAR(36) REFERENCES Drug_id,
+Dosage_drug2 TEXT,
+Drug3 VARCHAR(36) REFERENCES Drug_id,
+Dosage_drug3 TEXT,
+Drug4 VARCHAR(36) REFERENCES Drug_id,
+Dosage_drug4 TEXT,
+Drug5 VARCHAR(36) REFERENCES Drug_id,
+Dosage_drug5 TEXT,
+Date_given DATE
 );
 
 CREATE TABLE Patient(
